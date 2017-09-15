92b1e346fc855a183db01c6f99fb69e9bf40ed6d
Christian Nockemann <c.nockemann@lvm.de>
2017-09-15 09:21:52 +0200

    Weiter mit ParallelStreams

--- src/main/java/de/viadee/lambda/codebeispiele/performance/ParallelStreams.java
+++ src/main/java/de/viadee/lambda/codebeispiele/performance/ParallelStreams.java
@@ -6,16 +6,15 @@ import java.util.stream.Stream;
 public class ParallelStreams {
 
 	public static void main(String[] args) throws InterruptedException {
-		Integer[] zehnLaeufe = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).toArray(Integer[]::new);
 
 		final long startSequentiell = System.currentTimeMillis();
-		Stream.of(zehnLaeufe).forEach(warten(startSequentiell, "sequentiell"));
+		Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).forEach(warten(startSequentiell, "sequentiell"));
 
 		System.out
 				.println("==========================================================================================");
 
 		final long startParallel = System.currentTimeMillis();
-		Stream.of(zehnLaeufe).parallel().forEach(warten(startParallel, "parallel"));
+		Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).parallel().forEach(warten(startParallel, "parallel"));
 	}
 
 	private static Consumer<? super Integer> warten(final long startSeq, String typ) {

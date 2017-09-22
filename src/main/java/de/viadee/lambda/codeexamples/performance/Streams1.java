e75bd934a55d04fdbf844723448de422a71bbafa
NoC <NoC@NBW7W401.intern.viadee.de>
2017-09-22 18:09:24 +0200

    Stream Performance more Examples

--- src/main/java/de/viadee/lambda/codeexamples/performance/Streams1.java
+++ src/main/java/de/viadee/lambda/codeexamples/performance/Streams1.java
@@ -1,17 +1,18 @@
 package de.viadee.lambda.codeexamples.performance;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
-public class Streams {
+public class Streams1 {
 
 	public static void main(String[] args) {
 		List<Integer> ints = new Random()
 				.ints(100000, 0, Integer.MAX_VALUE)
 				.boxed()
 				.collect(Collectors.toList());
+
 		long startForEach = System.currentTimeMillis();
 		repeatForEach(ints, 1000);
 		long endForEach = System.currentTimeMillis();
@@ -19,13 +20,12 @@ public class Streams {
 		long startStream = System.currentTimeMillis();
 		repeatStream(ints, 1000);
 		long endStream = System.currentTimeMillis();
-		
-		IntStream intsPrim = new Random()
-				.ints(100000, 0, Integer.MAX_VALUE);
+
+		int[] intArray = new Random()
+				.ints(100000, 0, Integer.MAX_VALUE).toArray();
 		long startIntStream = System.currentTimeMillis();
-		repeatIntStream(intsPrim, 1000);
+		repeatIntStream(intArray, 1000);
 		long endIntStream = System.currentTimeMillis();
-		
 
 		System.out.println("Runtime ForEach: " + (endForEach - startForEach) + "ms");
 		System.out.println("Runtime Stream: " + (endStream - startStream) + "ms");
@@ -41,19 +41,19 @@ public class Streams {
 	}
 
 	private static int lambdaMaxInteger(List<Integer> ints) {
-		return ints.stream().reduce(Integer.MIN_VALUE, (a, b) -> Integer.max(a, b));
+		return ints.stream().reduce(Integer::max).get();
 	}
 
-	private static int lambdaMaxIntStream(IntStream ints) {
-		return ints.max().getAsInt();
+	private static int lambdaMaxIntStream(int[] intArray) {
+		return Arrays.stream(intArray).max().getAsInt();
 	}
 
-	private static void repeatIntStream(IntStream ints, int times) {
+	private static void repeatIntStream(int[] intArray, int times) {
 		for (int i = 0; i <= times; i++) {
-			lambdaMaxIntStream(ints);
+			lambdaMaxIntStream(intArray);
 		}
 	}
-	
+
 	private static void repeatForEach(List<Integer> ints, int times) {
 		for (int i = 0; i <= times; i++) {
 			forEachLoopMaxInteger(ints);
